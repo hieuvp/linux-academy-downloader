7359723b2f1916ec6db62ae3e5b2029f3350f9bd
Harrison Van <harrison.van@shopback.com>
2020-05-06 15:47:46 +0700

    network data

--- src/scraper/lesson.js
+++ src/scraper/lesson.js
@@ -19,15 +19,15 @@ module.exports = async (driver, url) => {
   await driver.wait(until.elementLocated(By.xpath('//video[starts-with(@src, \'blob:https://linuxacademy.com/\')]')), timeout);
 
   const entries = await driver.manage().logs().get(logging.Type.PERFORMANCE);
-  const found = entries.map((entry) => parseNetworkRequest(entry))
-    .find(request => {
-      return (
-        request.method === 'Network.requestWillBeSent'
+  const found = entries.map(entry => parseNetworkRequest(entry))
+    .find(request => (
+      request.method === 'Network.requestWillBeSent'
         && isNil(request.url) === false
         && request.url.startsWith('https://video-cdn.linuxacademy.com/')
         && request.url.includes('playlist.m3u8')
-      );
-    });
+    ));
 
-  console.log('found =', get(found, ['url']));
+  // console.log('found =', get(found, ['url']));
+
+  console.log(JSON.stringify(entries));
 };
