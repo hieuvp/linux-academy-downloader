7cad22328046b14a715bee42b4d0e3ccb641f0b3
Harrison Van <harrison.van@shopback.com>
2020-05-07 12:01:48 +0700

    typo

--- src/scraper/parser.test.js
+++ src/scraper/parser.test.js
@@ -4,16 +4,16 @@ const { parseCourse } = require('./parser');
 
 describe('parseCourse', () => {
   it('should accurately return items about course AWS Essentials', () => {
-    // Course: Mastering Systemd
-    // https://linuxacademy.com/cp/modules/view/id/171
+    // Course: AWS Essentials
+    // https://linuxacademy.com/cp/modules/view/id/241
     const html = readFileSync(`${__dirname}/test-data/course-aws-essentials.html`, 'utf8');
 
     expect(parseCourse(html)).toMatchSnapshot();
   });
 
   it('should accurately return items about course Mastering Systemd', () => {
-    // Course: AWS Essentials
-    // https://linuxacademy.com/cp/modules/view/id/241
+    // Course: Mastering Systemd
+    // https://linuxacademy.com/cp/modules/view/id/171
     const html = readFileSync(`${__dirname}/test-data/course-mastering-systemd.html`, 'utf8');
 
     expect(parseCourse(html)).toMatchSnapshot();
